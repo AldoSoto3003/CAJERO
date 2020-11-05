ae1acbb3e36046dcf0d2d84c3cae336a69fe3543
AldoSoto3003 <aldosoto3003@gmail.com>
2020-11-04 20:43:05 -0700

    ejercicios

---
+++ EJERCICIOS/bool.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+int main(){
+
+    std::cout << true << '\n';
+    std::cout << false << '\n';
+
+    std::cout << std::boolalpha ; // imprime bools como true o false
+
+    std::cout << true << '\n';
+    std::cout << false << '\n';
+
+
+}
