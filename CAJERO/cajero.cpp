e3b6cd5ffeefe25a90a79289f6d9ec970e453b24
AldoSoto3003 <aldosoto3003@gmail.com>
2020-11-20 00:51:49 -0700

    Avance Cajero

--- CAJERO/cajero.cpp
+++ CAJERO/cajero.cpp
@@ -2,15 +2,14 @@
 #include<string>
 #include<conio.h>
   //empiezo declaración de variables goblaes
-    char *nombre[] = {"aldo soto" , "Fernando", "Gabriel" };
-    int noCuenta[3];
-    int passCuenta[3]; 
-    int fondoCuenta1[3] = {100, 200, 300};
-    int key{}, aux {0};
-  //Termino declaración de 1varaibles globales
+    std::string nombres[3] = {"Aldo Soto", "Gabriel Valdez", "Ruben Berrelleza"};
+    int noCuenta[3] = {123, 456, 789};
+    int passCuenta[3] = {123, 456 ,789}; 
+    int fondoCuenta[3] = {100, 200, 300};
+    int key{}, aux {0}, pivote {}, opcion{};
+
+  //Termino declaración de 1varaibles globales  
    
-
-
 int login (){
     //variables locales
     int numeroCuenta{}, nip {};
@@ -18,43 +17,104 @@ int login (){
     std::cout<<"Ingrese su numero de cuenta"<<'\n';
     std::cin >> numeroCuenta;
 
-
     std::cout<<"Ingrese su nip"<<'\n';
     std::cin >> nip;
 
-    for (int i=0; i < 3, i++;){
-     
-        if ((numeroCuenta == noCuenta[i])  && (nip == passCuenta[i])){
-            aux = 1;
-            break;
-         }
+    for(int i=0; i <= 3; i+=1){
 
+         if (numeroCuenta == noCuenta[i]){
+             if(nip == passCuenta[i]){
+                 pivote = i;
+                 aux = 1;
+             }
+        }
     }
     return 0;
 }
 
+int consultaDeSaldo(){
+
+    std::cout <<"Hola estimado "  << nombres[pivote] << '\n';
+    std::cout <<"su saldo actualmente es de: " << fondoCuenta[pivote] << '\n';
+
+    std::cout << "desea realizar otra opcion ?" << '\n';
+    std::cout << "1.- SI    2.- NO"<< '\n';
+    std::cin >> opcion;
+   
+    return 0;
+}
+
+int retiro(){
+    int montoRetirar {};
+
+    std::cout <<" ingrese el monto a retirar: " << '\n';
+    std::cin >> montoRetirar;
+
+    if (montoRetirar > fondoCuenta[pivote]){
+        std::cout << "Fondos insufucientes" << '\n';
+    }else{ 
+        fondoCuenta[pivote] -= montoRetirar;
+        std::cout << "Retiro realizado con exito! " << '\n';
+    }
+
+    std::cout << "                 " << '\n';
+    std::cout << "desea realizar otra opcion ?" << '\n';
+    std::cout << "1.- SI    2.- NO"<< '\n';
+    std::cin >> opcion;
+
+    return 0;
+}
+
+int trasnferencia(){
+
+    int cuentaTransferir {}, montoTransferir {}, pivote2{};
+
+    std::cout << "Ingrese numero de cuenta a transferir " << '\n';
+    std::cin >> cuentaTransferir;
+
+    if(cuentaTransferir == noCuenta[pivote]){
+        std::cout << "No se puede transferir a su misma cuenta" << '\n';
+        exit(-1);
+    }
+
+    for(int i = 0; i < 3; i+=1){
+        if (cuentaTransferir == noCuenta[i]){
+            pivote2 = i;
+            std::cout << "Ingrese monto a trasnferir ";
+            std::cin >> montoTransferir;
+
+            if (montoTransferir <= fondoCuenta[pivote]){
+                fondoCuenta[pivote] -= montoTransferir;
+                fondoCuenta[pivote2] += montoTransferir;
+
+            }else if (montoTransferir > fondoCuenta[pivote]){
+                std::cout << "Fondos insuficientes" << '\n';
+            }
+            
+        }else if (i == 3){
+            std::cout << "Ingrese un numero de cuenta valido "<< '\n';      
+            i=4;   
+        }
+    }
+    
+    std::cout << "                 " << '\n';
+    std::cout << "desea realizar otra opcion ?" << '\n';
+    std::cout << "1.- SI    2.- NO"<< '\n';
+    std::cin >> opcion;
+    return 0;
+}
 
 
-int main(){
-    noCuenta[0] = {111};
-    passCuenta[0] = {444};
+int menu(){
 
-    login();
-
-   if (aux == 0){
-       std::cout<<"                             "<<'\n';
-       std::cout<<"Numero de cuenta y/o password incorrectas"<<'\n';
-       getch();
-       exit(-1);
-   }
-
-    //Menú del programa 
+     //Menú del programa 
     std::cout<<"Seleccione la opción que desea realizar:"<<'\n';
     std::cout<<"1.- consulta de saldo "<< '\n';
     std::cout<<"2.- retiro "<< '\n';
     std::cout<<"3.- deposito "<< '\n';
     std::cout<<"4.- trasnferencia "<< '\n';
     std::cout<<"5.- cambio de nip "<< '\n';
+    std::cout<<"6.- salir"<< '\n';
 
     std::cin >> key;
     
@@ -67,23 +127,61 @@ int main(){
     
     switch (key){
         case 1:
-          
+            consultaDeSaldo();
+            
+            if (opcion == 1 )
+                 menu();
+            else
+                exit(-1);  
+
         break;
+       
         case 2:
+
+            retiro();
+
+            if (opcion == 1 )
+                menu();
+            else
+                exit(-1);  
 
         break;
         case 3:
 
+           
+            
         break;
         case 4:
 
+             trasnferencia();
+
+            if (opcion == 1 )
+                menu();
+            else
+                exit(-1);
         break;
         case 5:
 
         break;
-
+  
     }
+    return 0;
+}
+
+int main(){
+
+    login(); //Aqui llamo a la funcion que se encarga de verificar las credenciales del usuario
+
+   if (aux == 0){
+       std::cout<<"                             "<<'\n';
+       std::cout<<"Numero de cuenta y/o password incorrectas"<<'\n';
+       getch();
+       exit(-1);
+   }
+
+    menu(); //llamo a la funcion menu, para que el usuario seleccione lo que desea hacer 
+   
     getch();
-    return 0  ;
+    return 0 ;
 }
 
