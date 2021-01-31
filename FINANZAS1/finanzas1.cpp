b8be2a522e76e8f763b4d11b807f354592c95384
AldoSoto3003 <aldosoto3003@gmail.com>
2021-01-31 01:51:31 -0700

    avance finanzas

--- FINANZAS1/finanzas1.cpp
+++ FINANZAS1/finanzas1.cpp
@@ -7,18 +7,20 @@
 #include <ctime>
 #include <iomanip>
 
-int main();
+int main(),otraOpcion();
 void menu(),ingreso(),validacionUsuario(),sacarHora(),mostrarTransacciones();
+void subGastos(), subTransferencias();
 std::string intToString();
 
 // INICIA DECLARACION DE VARIABLES GLOBALES
 int noCuenta [3] {123,456,789};
 int passCuenta[3] {123,456,789};
-int ingresoTotal[3] {};
+int gastoAlimentacion[3]{}, gastoEducacion[3]{}, gastoEntretenimiento[3]{},gastoSalud[3]{},gastoTransporte[3]{},gastoOtros[3]{};
+int ingresoTotal[3] {}, gastoTotal[3] {};
 std::string nombreUsuario[3] = {"Aldo","Ruben","Carlos"}, version{"1.0"};
 std::string listaTransacciones[10000]{};
 std::string fechaHora{};
-int billetera[3]{}, tarjeta[3] {}, ahorro[3] {}, bandera[3] {0,0,0};
+int billetera[3]{100,100,100}, tarjeta[3] {100,100,100}, ahorro[3] {100,100,100}, bandera[3] {0,0,0};
 int pivote{}, pivoteLista{0};
 
 std::string IntToString(int intValor) {
@@ -46,15 +48,18 @@ return(strRetVal);
 
 void menu(){
     int opcion{};
+    gastoTotal[pivote] = gastoAlimentacion[pivote] + gastoEducacion[pivote] + gastoEntretenimiento[pivote] + gastoSalud[pivote] + gastoTransporte[pivote] + gastoOtros[pivote];
     system("cls");
     std::cout << "Bienvenido a la app FINANZAS PERSONALES Sr. "<< nombreUsuario[pivote] << '\n';
     std::cout << "    \n";
-    std::cout << "   \n";
+    std::cout << "                                                            ||DINERO EN BILLETERA: $" << billetera[pivote] <<'\n';
+    std::cout << "                                                            ||DINERO EN TARJETA:   $" << tarjeta[pivote] << '\n';
+    std::cout << "                                                            ||DINERO EN AHORROS:   $" << ahorro[pivote] << '\n';
     std::cout <<"       SELECCIONE UNA OPCION:       \n";
     std::cout <<"1.- INGRESO                                                  ||INGRESOS:  |$"<< ingresoTotal[pivote] << '\n'  ;
-    std::cout <<"2.- GASTO                                                    ||GASTOS:    |$0.00\n";
+    std::cout <<"2.- GASTO                                                    ||GASTOS:    |$"<< gastoTotal[pivote] <<'\n';
     std::cout <<"3.- TRANSFERENCIA                                            ||...........|.....................\n";
-    std::cout <<"4.- REGISTRO DE OPERACIONES                                  ||TOTAL:     |$0.00\n";
+    std::cout <<"4.- REGISTRO DE OPERACIONES                                  ||TOTAL:     |$"<< ingresoTotal[pivote] - gastoTotal[pivote] <<'\n';
     std::cout <<"5.- CAMBIAR DE USUARIO\n";
     std::cout <<"6.- SALIR\n";
     std::cout <<"\n";
@@ -66,8 +71,20 @@ void menu(){
             ingreso();
         break;
 
+        case 2:
+            subGastos();
+        break;
+
+        case 3:
+            subTransferencias();
+        break;
+
         case 4:
             mostrarTransacciones();
+        break;
+
+        case 6:
+            main();
         break;
     }
 
@@ -76,17 +93,26 @@ void menu(){
 
 void mostrarTransacciones(){
 
+    system("cls");
+
+    std::cout << "Total de gastos en ALIMENTOS: $" << gastoAlimentacion[pivote] << '\n';
+    std::cout << "Total de gastos en EDUCACION: $" << gastoEducacion[pivote] << '\n';
+    std::cout << "Total de gastos en ENTRETENIMIENTO: $" << gastoEntretenimiento[pivote] << '\n';
+    std::cout << "Total de gastos en SALUD: $" << gastoSalud[pivote] << '\n';
+    std::cout << "Total de gastos en TRANSPORTE: $" << gastoTransporte[pivote] << '\n';
+    std::cout << "Total de gastos en OTROS: $" << gastoOtros[pivote] << '\n';
 
     if (listaTransacciones[0] == ""){
         std::cout << "No hay transacicones\n";
         system("pause");
         menu();
     }else{
+        std::cout << "Transacciones generadas hasta el dia y hora: " + fechaHora << '\n';
         for (int i=0 ; i<=10000; i++){
             if (listaTransacciones[i] == ""){
                 std::cout << "**FIN DE TRANSACCIONES**\n";
                 system("pause");
-                main();
+                menu();
             }
             std::cout << listaTransacciones[i] << '\n';
         }
@@ -96,7 +122,8 @@ void mostrarTransacciones(){
 
 void ingreso(){
 
-    int opcionCategoria{}, opcion2{}, dineroIngreso{};
+    int opcionCategoria{}, opcion2{};
+    int dineroIngreso{};
 retornoCategoria:
     system("cls");
     std::cout << "Seleccione categoria:\n";
@@ -176,7 +203,7 @@ retornoCuantoGuardar:
                 pivoteLista += 1; 
             }
             std::cout << "Transaccion dada de alta!\n";
-            ingresoTotal[pivote] += dineroIngreso;           
+           ingresoTotal[pivote]  += dineroIngreso;         
             menu();
         break;
 
@@ -195,7 +222,7 @@ retornoCuantoGuardar:
                 pivoteLista += 1; 
             }
             std::cout << "Transaccion dada de alta!\n";
-            ingresoTotal[pivote] += dineroIngreso;
+             ingresoTotal[pivote]  += dineroIngreso;  
             menu();
         break;
 
@@ -214,7 +241,8 @@ retornoCuantoGuardar:
                 pivoteLista += 1; 
             }
             std::cout << "Transaccion dada de alta!\n";
-            ingresoTotal[pivote] += dineroIngreso;
+             ingresoTotal[pivote]  += dineroIngreso;  
+            system("pause");
             menu();
         break;
 
@@ -268,6 +296,7 @@ void validacionUsuario (){
         system("pause");
         validacionUsuario();
     }else{
+        ingresoTotal[pivote] = billetera[pivote] + tarjeta[pivote] + ahorro[pivote];
         menu();
     }
 
@@ -285,7 +314,303 @@ void sacarHora(){
 
 }
 
+void subGastos(){
+    system("cls");
+    int opcionCategoria{},opcionPago{};
+    int dineroGasto{};
+    std::string otroGastoDescrip{};
 
+    std::cout << "Seleccione categoria gasto:  \n";
+    std::cout << "1.- ALIMENTACION \n";
+    std::cout << "2.- EDUCACION \n";
+    std::cout << "3.- ENTRETENIMIENTO \n";
+    std::cout << "4.- SALUD  \n";
+    std::cout << "5.- TRANSPORTE \n";
+    std::cout << "6.- OTROS \n";
+    std::cout << "Ingrese: ";
+
+    std::cin >> opcionCategoria;
+    if (opcionCategoria < 1 || opcionCategoria >6){
+        std::cout << "Ingrese opcion valida!!\n";
+        std::cout << "\n";
+        std::cout << "vuelva a intentarlo\n";
+        subGastos();
+    }
+retornoMetodoPago:
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "Seleccione metodo de pago: \n";
+    std::cout << "1.- BILLETERA\n";
+    std::cout << "2.- TARJETA\n";
+    std::cout << "3.- AHORROS\n";
+    std::cout << "Ingrese: ";
+    std::cin >> opcionPago;
+
+    if (opcionPago < 1 || opcionPago > 3){
+        std::cout << "Ingrese opcion valida!!\n";
+        std::cout << "\n";
+        std::cout << "vuelva a intentarlo\n";
+        system("pause");
+        system("cls");
+        goto retornoMetodoPago;
+    }
+retornoDigiteGasto:
+    std::cout << "\n";
+    std::cout << "Ingrese monto gastado: $";
+    std::cin >> dineroGasto;
+    if (dineroGasto <= 0 ){
+        std::cout << "La cantidad no puede ser menor o igual que 0\n";
+        std::cout << "Vuelva a intentarlo\n";
+        system("pause");
+        system("cls");
+        goto retornoDigiteGasto;
+    }else if(billetera[pivote] < dineroGasto || tarjeta[pivote] < dineroGasto || ahorro[pivote] < dineroGasto){
+        std::cout << "monto incorrecto: tu gasto no puede ser mayor que tus ingresos\n";
+        std::cout << "Vuelva a intentarlo\n";
+        system("pause");
+        system("cls");
+        goto retornoDigiteGasto;
+    }
+
+    switch (opcionCategoria){
+        case 1:
+            if (opcionPago == 1){
+                billetera[pivote] -= dineroGasto;
+                listaTransacciones[pivoteLista] = "Gasto de: " + IntToString(dineroGasto) + " en: BILLETERA por ALIMENTOS con fecha de: " + fechaHora;
+                pivoteLista += 1;
+            }else if(opcionPago == 2){
+                tarjeta[pivote] -= dineroGasto;
+                listaTransacciones[pivoteLista] = "Gasto de: " + IntToString(dineroGasto) + " en: TARJETA por ALIMENTOS con fecha de: " + fechaHora;
+                pivoteLista += 1;
+            }else{
+                ahorro[pivote] -= dineroGasto;
+                listaTransacciones[pivoteLista] = "Gasto de: " + IntToString(dineroGasto) + " en: AHORROS por ALIMENTOS con fecha de: " + fechaHora;
+                pivoteLista += 1;
+            }
+            gastoAlimentacion[pivote] += dineroGasto;
+        break;
+
+        case 2:
+            if (opcionPago == 1){
+                billetera[pivote] -= dineroGasto;
+                listaTransacciones[pivoteLista] = "Gasto de: " + IntToString(dineroGasto) + " en: BILLETERA por EDUCACION con fecha de: " + fechaHora;
+                pivoteLista += 1;
+            }else if(opcionPago == 2){
+                tarjeta[pivote] -= dineroGasto;
+                listaTransacciones[pivoteLista] = "Gasto de: " + IntToString(dineroGasto) + " en: TARJETA por EDUCACION con fecha de: " + fechaHora;
+                pivoteLista += 1;
+            }else{
+                ahorro[pivote] -= dineroGasto;
+                listaTransacciones[pivoteLista] = "Gasto de: " + IntToString(dineroGasto) + " en: AHORROS por EDUCACION con fecha de: " + fechaHora;
+                pivoteLista += 1;
+            }
+            gastoEducacion[pivote] += dineroGasto;
+        break;
+
+        case 3:
+            if (opcionPago == 1){
+                billetera[pivote] -= dineroGasto;
+                listaTransacciones[pivoteLista] = "Gasto de: " + IntToString(dineroGasto) + " en: BILLETERA por ENTRETENIMIENTO con fecha de: " + fechaHora;
+                pivoteLista += 1;
+            }else if(opcionPago == 2){
+                tarjeta[pivote] -= dineroGasto;
+                listaTransacciones[pivoteLista] = "Gasto de: " + IntToString(dineroGasto) + " en: TARJETA por ENTRETENIMIENTO con fecha de: " + fechaHora;
+                pivoteLista += 1;
+            }else{
+                ahorro[pivote] -= dineroGasto;
+                listaTransacciones[pivoteLista] = "Gasto de: " + IntToString(dineroGasto) + " en: AHORROS por ENTRETENIMIENTO con fecha de: " + fechaHora;
+                pivoteLista += 1;
+            }
+            gastoEntretenimiento[pivote] += dineroGasto;
+        break;
+
+        case 4:
+            if (opcionPago == 1){
+                billetera[pivote] -= dineroGasto;
+                listaTransacciones[pivoteLista] = "Gasto de: " + IntToString(dineroGasto) + " en: BILLETERA por SALUD con fecha de: " + fechaHora;
+                pivoteLista += 1;
+            }else if(opcionPago == 2){
+                tarjeta[pivote] -= dineroGasto;
+                listaTransacciones[pivoteLista] = "Gasto de: " + IntToString(dineroGasto) + " en: TARJETA por SALUD con fecha de: " + fechaHora;
+                pivoteLista += 1;
+            }else{
+                ahorro[pivote] -= dineroGasto;
+                listaTransacciones[pivoteLista] = "Gasto de: " + IntToString(dineroGasto) + " en: AHORROS por SALUD con fecha de: " + fechaHora;
+                pivoteLista += 1;
+            }
+            gastoSalud[pivote] += dineroGasto;
+        break;
+
+        case 5:
+            if (opcionPago == 1){
+                billetera[pivote] -= dineroGasto;
+                listaTransacciones[pivoteLista] = "Gasto de: " + IntToString(dineroGasto) + " en: BILLETERA por TRANSPORTE con fecha de: " + fechaHora;
+                pivoteLista += 1;
+            }else if(opcionPago == 2){
+                tarjeta[pivote] -= dineroGasto;
+                listaTransacciones[pivoteLista] = "Gasto de: " + IntToString(dineroGasto) + " en: TARJETA por TRANSPORTE con fecha de: " + fechaHora;
+                pivoteLista += 1;
+            }else{
+                ahorro[pivote] -= dineroGasto;
+                listaTransacciones[pivoteLista] = "Gasto de: " + IntToString(dineroGasto) + " en: AHORROS por TRANSPORTE con fecha de: " + fechaHora;
+                pivoteLista += 1;
+            }
+            gastoTransporte[pivote] += dineroGasto;
+        break;
+
+        case 6:
+            std::cout <<"\n";
+            std::cout << "Ingrese breve descripcion del tipo de gastos: ";
+            std::cin >> otroGastoDescrip;
+             if (opcionPago == 1){
+                billetera[pivote] -= dineroGasto;
+                listaTransacciones[pivoteLista] = "Gasto de: " + IntToString(dineroGasto) + " en: BILLETERA por " + otroGastoDescrip + "con fecha de: " + fechaHora;
+                pivoteLista += 1;
+            }else if(opcionPago == 2){
+                tarjeta[pivote] -= dineroGasto;
+                listaTransacciones[pivoteLista] = "Gasto de: " + IntToString(dineroGasto) + " en: BILLETERA por " + otroGastoDescrip + "con fecha de: " + fechaHora;
+                pivoteLista += 1;
+            }else{
+                ahorro[pivote] -= dineroGasto;
+                listaTransacciones[pivoteLista] = "Gasto de: " + IntToString(dineroGasto) + " en: BILLETERA por " + otroGastoDescrip + "con fecha de: " + fechaHora;
+                pivoteLista += 1;
+            }
+            gastoOtros[pivote] += dineroGasto;
+        break;
+    }
+    
+
+menu();
+
+}
+
+void subTransferencias(){
+
+    int opcionOrigen{}, opcionDestino{},dineroTrasnf{};
+
+    system("cls");
+    std::cout << "Seleccione origen del dinero: \n";
+    std::cout << "1.- BILLETERA \n";
+    std::cout << "2.- TARJETA \n";
+    std::cout << "3.- AHORRO \n";
+    std::cin >> opcionOrigen;
+    if (opcionOrigen <1 || opcionOrigen > 3){
+        std::cout << "Debe ingresar una de las opciones correctas!\n";
+        std::cout << "vuelva a intentarlo!\n";
+        subTransferencias();
+    }
+
+    std::cout << "Seleccione destino del dinero: \n";
+    std::cout << "1.- BILLETERA \n";
+    std::cout << "2.- TARJETA \n";
+    std::cout << "3.- AHORRO \n";
+    std::cin >> opcionDestino;
+    if (opcionDestino <1 || opcionDestino > 3){
+        std::cout << "Debe ingresar una de las opciones correctas!\n";
+        std::cout << "vuelva a intentarlo!\n";
+        menu();
+    }
+
+    if (opcionOrigen == opcionDestino){
+        std::cout << "No se puede transferir a la misma cuenta!! \n";
+        std::cout << "Intentelo de nuevo!\n";
+        system("pause");
+        system("cls");
+         menu();
+    }
+retornoDigiteMonto:
+    std::cout << "\n";
+    std::cout << "Ingrese monto a transferir: $";
+    std::cin >> dineroTrasnf;
+
+    if (dineroTrasnf <= 0){
+        std::cout << "Digite una cantidad mayor que 0! \n";
+        std::cout << "vuelva a intentarlo!\n";
+        system("pause");
+        system("cls");
+        goto retornoDigiteMonto;
+    }
+
+    switch (opcionOrigen){
+        case 1: 
+            if(dineroTrasnf > billetera[pivote]){
+                std::cout << "Fondos insuficientes!\n";
+                system("pause");
+                menu();
+            }
+        break;
+
+        case 2: 
+            if(dineroTrasnf > tarjeta[pivote]){
+                std::cout << "Fondos insuficientes!\n";
+                system("pause");
+                menu();
+            }
+        break;
+
+        case 3: 
+            if(dineroTrasnf > ahorro[pivote]){
+                std::cout << "Fondos insuficientes!\n";
+                system("pause");
+                menu();
+            }
+        break;
+    }
+
+    switch (opcionOrigen){
+        case 1:
+            if (opcionDestino == 2){
+                tarjeta[pivote] += dineroTrasnf;
+                billetera[pivote] -= dineroTrasnf;
+            }else{
+                ahorro[pivote] += dineroTrasnf;
+                billetera[pivote] -= dineroTrasnf;
+            }
+        break;
+
+        case 2:
+            if (opcionDestino == 1){
+                billetera[pivote] += dineroTrasnf;
+                tarjeta[pivote] -= dineroTrasnf;
+            }else{
+                ahorro[pivote] += dineroTrasnf;
+                tarjeta[pivote] -= dineroTrasnf;
+            }
+        break;
+
+        case 3:
+            if (opcionDestino == 1){
+                billetera[pivote] += dineroTrasnf;
+                ahorro[pivote] -= dineroTrasnf;
+            }else{
+                tarjeta[pivote] += dineroTrasnf;
+                ahorro[pivote] -= dineroTrasnf;
+            }
+        break;
+    }
+menu();
+}
+
+int otraOpcion(){
+    int opcion {};
+    system("cls");
+    std::cout << "                 " << '\n';
+    std::cout << "desea realizar otra opcion ?" << '\n';
+    std::cout << "1.- SI    2.- NO"<< '\n';
+    std::cin >> opcion;
+
+    if (opcion == 1){
+        menu();
+    }else if (opcion == 2){
+        validacionUsuario();
+    }else{
+        std::cout << "Opcion no valida " << '\n';
+        std::cout << "Vuelva a intentarlo " << '\n'; 
+        otraOpcion();
+    }
+    system("pause");
+    return 0;
+}
 
 int main(){
 
